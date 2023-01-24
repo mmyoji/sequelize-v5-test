c413a2fba7f7f26892f7ab3e4f110a0e4dab638d
mmyoji <mmyoji@gmail.com>
2023-01-25 08:36:50 +0900

    style: tweaks

--- main.ts
+++ main.ts
@@ -1,17 +1,37 @@
-const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('sqlite::memory:');
+import { Sequelize, Model, DataTypes, Op } from "sequelize";
+
+const sequelize = new Sequelize("sqlite::memory:");
 
 class User extends Model {}
-User.init({
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE
-}, { sequelize, modelName: 'user' });
 
-sequelize.sync()
-  .then(() => User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  }))
-  .then(jane => {
-    console.log(jane.toJSON());
+User.init(
+  {
+    username: DataTypes.STRING,
+    age: { type: DataTypes.INTEGER, allowNull: true },
+    birthday: DataTypes.DATE,
+  },
+  { sequelize, modelName: "user" }
+);
+
+async function main() {
+  await sequelize.sync();
+
+  const user = await User.create({
+    username: "janedoe",
+    birthday: new Date(1980, 6, 20),
   });
+
+  console.log(user.toJSON());
+
+  const users = await User.findAll({
+    where: {
+      age: {
+        [Op.ne]: null,
+      },
+    },
+  });
+
+  console.log(users.map((u) => u.toJSON()));
+}
+
+main().catch(console.error);
